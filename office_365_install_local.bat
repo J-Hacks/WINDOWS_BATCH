d49390196aa6f19a7597980412a64eb61693ec7d
J-Hacks <132538963+J-Hacks@users.noreply.github.com>
2024-05-30 07:48:32 +0530

    Update office_365_install_local.bat

--- office_365_install_local.bat
+++ office_365_install_local.bat
@@ -1,3 +1,4 @@
 @echo off
 set networkPath=\\192.168.0.232\ms
+REM get config file from xml file
 %networkPath%\OfficeDeployment\setup.exe /configure %networkPath%\OfficeDeployment\configuration-Office365-x64_share.xml
